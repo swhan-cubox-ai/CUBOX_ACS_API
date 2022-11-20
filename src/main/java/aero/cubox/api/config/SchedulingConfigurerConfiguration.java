0cf01b380b1e32adda17b2bd292e86d0677a49db
ray cho <ray7.cho@gmail.com>
2022-11-20 15:48:27 +0900

    feat: 스케쥴 Pool Size

---
+++ src/main/java/aero/cubox/api/config/SchedulingConfigurerConfiguration.java
@@ -0,0 +1,19 @@
+package aero.cubox.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+@Configuration
+public class SchedulingConfigurerConfiguration implements SchedulingConfigurer {
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        // DEPT, EMPCARD_10S, EMPCARD_10M, FILE, FEATURE
+        taskScheduler.setPoolSize(5);
+        taskScheduler.initialize();
+        taskRegistrar.setTaskScheduler(taskScheduler);
+    }
+}
