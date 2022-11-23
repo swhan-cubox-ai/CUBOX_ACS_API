739628b1d20bd041605d885e2559a34b84b5fb2a
ray cho <ray7.cho@gmail.com>
2022-11-23 20:21:28 +0900

    feat: T_ENT_HIST

--- src/main/java/aero/cubox/api/domain/entity/EntHist.java
+++ src/main/java/aero/cubox/api/domain/entity/EntHist.java
@@ -6,6 +6,7 @@ import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.sql.Time;
 import java.sql.Timestamp;
 
 @Builder
@@ -23,54 +24,97 @@ public class EntHist implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     @Column(name = "id")
     private Integer id;
+
     @Column(name = "evt_dt")
     private Timestamp evtDt;
+
     @Column(name = "ent_evt_typ")
     private String entEvtTyp;
-    @Column(name = "terminal_id")
-    private Integer terminalId;
+
     @Column(name = "terminal_cd")
     private String terminalCd;
-    @Column(name = "terminal_typ")
-    private String terminalTyp;
-    @Column(name = "model_nm")
-    private String modelNm;
-    @Column(name = "mgmt_num")
-    private String mgmtNum;
-    @Column(name = "ip_addr")
-    private String ipAddr;
-    @Column(name = "complex_auth_typ")
-    private String complexAuthTyp;
-    @Column(name = "door_id")
-    private Integer doorId;
-    @Column(name = "door_nm")
-    private String doorNm;
-    @Column(name = "building_id")
-    private Integer buildingId;
-    @Column(name = "building_nm")
-    private String buildingNm;
-    @Column(name = "emp_id")
-    private Integer empId;
-    @Column(name = "emp_nm")
-    private String empNm;
+
     @Column(name = "emp_cd")
     private String empCd;
-    @Column(name = "emp_no")
-    private Integer empNo;
+
+    @Column(name = "emp_nm")
+    private String empNm;
+
+    @Column(name = "face_id")
+    private Integer faceId;
+
+    @Column(name = "card_no")
+    private String cardNo;
+
+    @Column(name = "card_class_typ")
+    private String cardClassTyp;
+
+    @Column(name = "card_state_typ")
+    private String cardStateTyp;
+
+    @Column(name = "card_tag_typ")
+    private String cardTagTyp;
+
+    @Column(name = "beg_dt")
+    private Timestamp begDt;
+
+    @Column(name = "end_dt")
+    private Timestamp endDt;
+
+    @Column(name = "auth_way_typ")
+    private String authWayTyp;
+
+    @Column(name = "match_score")
+    private Float matchScore;
+
+    @Column(name = "face_threshold")
+    private Float faceThreshold;
+
+    @Column(name = "capture_at")
+    private Timestamp captureAt;
+
+    @Column(name = "tag_at")
+    private Timestamp tagAt;
+
+    @Column(name = "tag_card_no")
+    private Float tagCardNo;
+
+    @Column(name = "tag_emp_cd")
+    private Float tagEmpCd;
+
+    @Column(name = "temper")
+    private Float temper;
+
+    @Column(name = "mask_confidence")
+    private Float maskConfidence;
+
+    @Column(name = "terminal_typ")
+    private String terminalTyp;
+
+    @Column(name = "door_cd")
+    private String doorCd;
+
+    @Column(name = "door_nm")
+    private String doorNm;
+
+    @Column(name = "building_cd")
+    private String buildingCd;
+
+    @Column(name = "building_nm")
+    private String buildingNm;
+
     @Column(name = "dept_cd")
     private String deptCd;
+
     @Column(name = "dept_nm")
     private String deptNm;
-    @Column(name = "belong_cd")
-    private String belongCd;
-    @Column(name = "belong_nm")
-    private String belongNm;
-    @Column(name = "ent_face_img")
-    private byte[] entFaceImg;
+
     @Column(name = "created_at")
     private Timestamp createdAt;
+
     @Column(name = "updated_at")
     private Timestamp updatedAt;
 
